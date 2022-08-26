ff26b48879babd2df8c1f84564f1c5bdaa6c164a
1998-droid <tiotiotio98@gmail.com>
2022-08-26 09:26:51 +0700

    commit

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -2,7 +2,8 @@
     include_once "../_header.php";
 ?>
 
-       
+
+<script type="text/javascript" src="../assets/js/Chart.js"></script>
 <div id="layoutSidenav_content">
     <main>
         <div class="container-fluid px-4">
